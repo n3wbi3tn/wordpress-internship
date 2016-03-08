e55da7abf597e7f3fa5b0b58d029386f86db3a48
NajehM <mansouri.najeh@gmail.com>
2016-03-08 13:47:09 +0100

    modif

--- helloWorld.php
+++ helloWorld.php
@@ -8,12 +8,12 @@ Author URI: http://www.njh.com
 Version: 1.0
 */
 // This just echoes hello world
-function hello_wd() {
+function hello_wd()
+{
 	$h = "hello world";
 	echo "<p id='hw'>$h</p>";
 }
 
-// Now we set that function up to execute when the admin_notices action is called
 add_action( 'admin_notices', 'hello_wd' );
 
 ?>
